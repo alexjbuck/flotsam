b2ab68974a27fb2ab982e392f2ee865c2ad6951f
Alex Buck <alexjbuck@gmail.com>
2023-04-27 16:40:25 -0400

    fix: Force nickname db query to return singleton

--- src/routes/f/[nickname]/+server.ts
+++ src/routes/f/[nickname]/+server.ts
@@ -4,20 +4,29 @@ import type { RequestHandler } from "./$types.js";
 
 // Call this function to redirect to the new URL
 
-export const GET = (async ({ params, url }) => {
+export const GET = (async ({ params }) => {
   const { nickname } = params;
+  console.log(`? Request for ${nickname}`)
 
   const { data } = await supabase
     .from('url_nicknames')
     .select('url')
     .eq('nickname', nickname)
-  const res = data?.reduce((p, n) => { return n }, { url: '/' });
-  let redir_url = res?.url || '/';
+    .limit(1)
+    .maybeSingle()
+  
+  console.log(`${data?.url ? '✓' : 'x'} Found: ${data?.url}`)
 
+  let redir_url = data?.url || '/';
+
+  console.log(`↩️ Routing to ${redir_url}`)
+
+  if (redir_url==='/') {
+    throw redirect(307, redir_url)
+  }
+  
   if (!redir_url.includes('http')) {
     redir_url = 'http://' + redir_url;
-  }
-
-  throw redirect(308, redir_url);
-
+  }  
+  throw redirect(307, redir_url);
 }) satisfies RequestHandler
